7b7a5176c892daa103cea15b0519b86d5ef90615
alexs <alexshalimov2019@gmail.com>
2024-04-04 15:22:43 +0300

    Finish task number1

--- Shalimov_IKM-722a_Course_project/Form1.cs
+++ Shalimov_IKM-722a_Course_project/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,58 @@ namespace Shalimov_IKM_722a_Course_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode;
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Минуло 15 секунд", "Увага!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tClock.Start();
+                bStart.Text = "Стоп";
+                this.Mode = false;
+                tbInput.Enabled = true;
+                tbInput.Focus();
+            }
+            else
+            {
+                tClock.Stop();
+                bStart.Text = "Пуск";
+                this.Mode = true;
+            }
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop() ;
+                MessageBox.Show("Неправильнинй символ","Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
